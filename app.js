b53e9ea1f1f717f708b49b75a1f4860f8d709ce6
urth <djolecs97@gmail.com>
2018-07-25 22:04:48 +0200

    asd

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(function(req, res, next){
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://fierce-fortress-40988.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
